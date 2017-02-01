94da939ddef3e1149cea9e7257c6060e190fbf97
James Gilders <jamesapaloosa1@gmail.com>
2017-02-01 12:47:27 -0700

    Functioning input
    -Adds menu
    - can choose strategy
    -human strategy can be fed movement inputs

--- Askinput.hs
+++ Askinput.hs
@@ -30,19 +30,47 @@ ranger k (x:xs) =
        then ranger (k-1) xs
        else False
 
+fetch :: String -> Int
+fetch (x:xs) = digitToInt x
 
-pMenu :: IO String
-pMenu = do
+
+pMenuW :: IO String
+pMenuW = do
+   putStr "Please enter strategy for white player\n"
    putStr "1 Aggression Strategy\n"
    putStr "2 Passive Strategy\n"
    putStr "3 Daniel's Super Smart Strategy\n"
    putStr "4 human strategy\n\n"
    getLine
 
+pMenuB :: IO String
+pMenuB = do
+   putStr "Please enter strategy for black player\n"
+   putStr "1 Aggression Strategy\n"
+   putStr "2 Passive Strategy\n"
+   putStr "3 Daniel's Super Smart Strategy\n"
+   putStr "4 human strategy\n\n"
+   getLine
 
-main = do
-   choice <- pMenu
+humanMove :: IO [Int]
+humanMove = do 
    line1 <- promptLine "Enter the move coordinate for player White in the form 'srcX srcY destX destY 0 <= n < 5, or just enter return for a pass W2: "
-           if ranger  4 ((seperate line1) []) == False
-             then main
-             else putStr "Valid input.\n"
+   if ranger 4 ((seperate line1) []) == False
+      then humanMove
+      else return ((seperate line1) [])
+   
+   
+manageStrat :: Int -> IO [Int]
+manageStrat 1 = manageStrat 1
+manageStrat 2 = manageStrat 2
+manageStrat 3 = manageStrat 3
+manageStrat 4 = do 
+    move <- humanMove
+    return move
+
+getInput :: IO [Int]
+getInput = do
+   choice1 <- pMenuW
+   choice2 <- pMenuB
+   moves <- manageStrat (fetch choice1)
+   return moves
