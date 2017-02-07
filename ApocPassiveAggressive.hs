012cef70ee3bcd585f4104db4248aeb0086c80bf
CarlinLiu <CarlinLiu@users.noreply.github.com>
2017-02-07 11:41:37 -0700

    Added PassiveAggressive

---
+++ ApocPassiveAggressive.hs
@@ -0,0 +1,63 @@
+--ApocPassiveAggressive.hs
+
+module ApocPassiveAggressive where
+
+import ApocTools
+import ApocUtility
+import ApocStrategyGreedy
+
+passAgg :: Chooser
+passAgg gs Normal p = do
+        let moves = (filter (killMove g) (validMoves player board))
+        in case (length moves) of
+            0 -> return $ Just Nothing
+            _ -> return $ Just moves !! 0 
+
+
+
+killMove :: GameState -> Played -> Bool
+killMove g (Played (src, dst)) = (getFromBoard (theBoard g) dst) \= E
+
+
+		
+--passAgg gs Played =  
+
+{- | 
+if kill move possible
+then execute
+else pass on turn
+-}
+
+{-
+getMoves :: Player -> [[a]] -> [(Int, Int)] -> [((Int, Int),(Int, Int))]
+getMoves player board ((x,y):xs)
+		| getBoardVal board (x,y) == BK = [((),())]  --how to do the +1,+2
+		| getBoardVal board (x,y) == BP = [((x,y),(x,y+1))]
+		| getBoardVal board (x,y) == WK = [((),())]
+		| getBoardVal board (x,y) == WP = [((x,y),(x,y-1))]
+-}
+
+--killMove :: Player -> [[cell]] -> [((Int, Int),(Int,Int))]
+--killMoves :: [Played] -> board
+--killMoves (((x,y),(p,q)):xs)
+	--	| getFromBoard board (p,q) == BK = [((x,y),(p,q))]
+	--	| getFromBoard board (p,q) == BP = [((x,y),(p,q))]
+	--	| getFromBoard board (p,q) == WK = [((x,y),(p,q))]
+	--	| getFromBoard board (p,q) == WP = [((x,y),(p,q))]
+	--	|otherwise []
+		 
+
+
+-- | Return the value (BK, BP etc.) at a point from the board
+--it takes in a 2d array and (x,y) and will return what is at that location
+--it will find the index of the xs and then find the index of the 
+
+--snd = (a,b) the b
+--fst = (a,b) the a
+
+--take the second of the touple and index it, then take the first of the touple and index that
+--move to apocTools
+--getBoardVal             :: [[a]] -> (Int,Int) -> a
+--getBoardVal xs pt       = xs !! fst pt !! snd pt
+
+
