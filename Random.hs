1f592d5ab747bbda4ed88a8da6521b055c830f27
James Gilders <jamesapaloosa1@gmail.com>
2017-02-07 13:59:54 -0700

    Add files via upload

--- Random.hs
+++ Random.hs
@@ -8,11 +8,13 @@ import System.Random
 
 randomChoice :: Chooser
 randomChoice g Normal p = do
-                ranNum <- (randomRIO (0, ((length (validMoves p g)) - 1))
-                (validMoves p g)!!ranNum
+                ranNum <- (randomRIO (-1, ((length (validMoves p g)) - 1))
+                if ranNum == -1
+                        then return Nothing
+                        else return just((validMoves p g)!!ranNum)
 randomChoice g PawnPlacement p = do
                 ranPlace <-(randomRIO(0, (length (ifEmpty 0 0 [] g)))-1))
-                return (ifEmpty 0 0 [] g)!!ranPlace
+                return just ((ifEmpty 0 0 [] g)!!ranPlace)
 
  
 ifEmtpy :: Int -> Int -> [(Int,Int)] -> GameState -> [(Int, Int)]
@@ -20,4 +22,3 @@ ifEmpty x 5 z g = ifEmpty (x + 1) 0 z g
 ifEmpty 5 y z g = z
 ifEmpty x y z g | getBoardVal (theBoard g) (x,y) == E = (x,y):z ifEmpty x (y + 1) z g
                 | otherwise = ifEmpty x (y + 1) z g
-
