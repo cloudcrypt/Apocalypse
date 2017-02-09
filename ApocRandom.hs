333b87ec83b2f4ecdc0d37f79e43cb7dde8004bf
James Gilders <jamesapaloosa1@gmail.com>
2017-02-09 11:33:03 -0700

    Update ApocRandom.hs

--- ApocRandom.hs
+++ ApocRandom.hs
@@ -1,4 +1,6 @@
+{- |
 
+-}
 module ApocRandom (
         randomChoice
         ) where
@@ -7,7 +9,7 @@ import ApocUtility
 import ApocTools
 import System.Random
 
--- |This generates random numbers.
+-- |This generates a random decesion which choose some moves or a pass 
 
 randomChoice :: Chooser
 randomChoice g Normal p = do
@@ -20,18 +22,13 @@ randomChoice g PawnPlacement p = do
                 let cells = emptyCells g
                 ranNum <- (randomRIO (0, ((length cells) - 1)))
                 return (Just [(cells !! ranNum)])
-                -- ranPlace <- (randomRIO(0, (length (ifEmpty 0 0 [] g)))-1)
-                -- return Just ((ifEmpty 0 0 [] g) !! ranPlace)
 
+-- | Sets the values a small array of two tuples so it can be taken by the function to return a random
 
 blah :: Played -> [(Int,Int)]
 blah (Played (src,dst)) = [src,dst]
- 
--- ifEmpty :: Int -> Int -> [(Int,Int)] -> GameState -> [(Int, Int)]
--- ifEmpty x 5 z g = ifEmpty (x + 1) 0 z g
--- ifEmpty 5 y z g = z
--- ifEmpty x y z g | getBoardVal (theBoard g) (x,y) == E = (x,y):z ifEmpty x (y + 1) z g
---                 | otherwise = ifEmpty x (y + 1) z g
+
+-- | Gets the empty cells so that the pawn placement can happen in a random location
 
 emptyCells :: GameState -> [(Int,Int)]
 emptyCells g = filter (\x -> (getFromBoard (theBoard g) x)==E) cells                    
