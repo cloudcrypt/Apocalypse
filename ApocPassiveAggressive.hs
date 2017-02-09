3188653338b7665f598b7362402e45402f672553
CarlinLiu <CarlinLiu@users.noreply.github.com>
2017-02-09 11:01:15 -0700

    Update ApocPassiveAggressive.hs

--- ApocPassiveAggressive.hs
+++ ApocPassiveAggressive.hs
@@ -6,61 +6,38 @@ module ApocPassiveAggressive(
 
 import ApocTools
 import ApocUtility
+import System.Random
 
+{- |This method will first filter out the killmoves and valid moves from the user
+sepcified board. It will then check the length of the list and if the list is empty
+(length 0) nothing is returned. If there is something, it will convert the moves to 
+a touple and will return. For pawnplacement, nothing is returned.
+-}
 passAgg :: Chooser
 passAgg gs Normal p = do
     let moves = (filter (killMove gs) (validMoves p gs))
+    let ranNum <- randomRIO(0, ((length moves) -1))
 	in case (length moves) of
         0 -> return $ Nothing
-        _ -> return $ Just (blah (moves !! 0))
+        _ -> return $ Just (blah (moves !! ranNum))
 passAgg gs PawnPlacement p = return Nothing
 
+{- |This method is used to convert a Played type into a touple
+-}
 blah :: Played -> [(Int,Int)]
 blah (Played (src,dst)) = [src,dst]
 
+
+{- | This function will look through the defined board and will check if the value is
+an empty character. If the character is empty it will return a boolean true.
+-}
 killMove :: GameState -> Played -> Bool
 killMove gs (Played (src, dst)) = (getFromBoard (theBoard gs) dst) /= E
 
 
-		
---passAgg gs Played =  
-
-{- | 
-if kill move possible
-then execute
-else pass on turn
--}
-
-{-
-getMoves :: Player -> [[a]] -> [(Int, Int)] -> [((Int, Int),(Int, Int))]
-getMoves player board ((x,y):xs)
-		| getBoardVal board (x,y) == BK = [((),())]  --how to do the +1,+2
-		| getBoardVal board (x,y) == BP = [((x,y),(x,y+1))]
-		| getBoardVal board (x,y) == WK = [((),())]
-		| getBoardVal board (x,y) == WP = [((x,y),(x,y-1))]
--}
-
---killMove :: Player -> [[cell]] -> [((Int, Int),(Int,Int))]
---killMoves :: [Played] -> board
---killMoves (((x,y),(p,q)):xs)
-	--	| getFromBoard board (p,q) == BK = [((x,y),(p,q))]
-	--	| getFromBoard board (p,q) == BP = [((x,y),(p,q))]
-	--	| getFromBoard board (p,q) == WK = [((x,y),(p,q))]
-	--	| getFromBoard board (p,q) == WP = [((x,y),(p,q))]
-	--	|otherwise []
-		 
 
 
--- | Return the value (BK, BP etc.) at a point from the board
---it takes in a 2d array and (x,y) and will return what is at that location
---it will find the index of the xs and then find the index of the 
 
---snd = (a,b) the b
---fst = (a,b) the a
 
---take the second of the touple and index it, then take the first of the touple and index that
---move to apocTools
---getBoardVal             :: [[a]] -> (Int,Int) -> a
---getBoardVal xs pt       = xs !! fst pt !! snd pt
 
 
