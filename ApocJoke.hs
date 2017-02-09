cb51c2b42410e49a05fe8d45a7772976686400f0
CarlinLiu <CarlinLiu@users.noreply.github.com>
2017-02-09 11:11:20 -0700

    Create ApocJoke.hs

---
+++ ApocJoke.hs
@@ -0,0 +1,14 @@
+--ApocJoke.hs
+
+module ApocPassiveAggressive(
+	joke
+	) where
+
+import ApocTools
+import ApocUtility
+import System.Random
+
+
+joke :: Chooser
+joke gs Normal p = return Nothing
+joke gs PawnPlacement p = return Nothing
